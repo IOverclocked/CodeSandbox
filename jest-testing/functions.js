1acd2d8e168609b0f0ae71d911ef1dde69cff019
IOverclocked <osiatycki.m@gamil.com>
2018-05-11 10:46:57 +0200

    Unit test obj

--- jest-testing/functions.js
+++ jest-testing/functions.js
@@ -1,6 +1,14 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
-    isNull: () => null
+    isNull: () => null,
+    checkValue: x => x,
+    user: () => {
+        const User = {
+            firstname: "Michał",
+            lastname: "Osiatycki"
+        }
+        return User;
+    }
 };
 
 module.exports = functions;
--- jest-testing/functions.test.js
+++ jest-testing/functions.test.js
@@ -11,3 +11,23 @@ test("Add: 2 + 2 not = 5", () => {
 test("Should be null", () => {
     expect(functions.isNull()).toBeNull();
 })
+
+test("Should be false", () => {
+    expect(functions.checkValue(null)).toBeFalsy()
+})
+
+test("Should be user Michał Osiatycki", () => {
+    expect(functions.user()).toEqual({
+        firstname: "Michał",
+        lastname: "Osiatycki"
+    })
+})
+
+//not works...
+/*
+test("Should be under 1600", () => {
+    const num1 = 800;
+    const num2 = 700;
+    expect(num1 + num2).toBeLessThen(1600);
+})
+*/
