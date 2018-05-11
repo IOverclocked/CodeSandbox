d6c8d880682d22bb0b21bfad5edb8c97341e5a25
IOverclocked <osiatycki.m@gamil.com>
2018-05-11 10:08:51 +0200

    be null unit test

--- jest-testing/functions.test.js
+++ jest-testing/functions.test.js
@@ -3,3 +3,11 @@ const functions = require('./functions');
 test("Add: 2 + 2 = 4", () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Add: 2 + 2 not = 5", () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+})
+
+test("Should be null", () => {
+    expect(functions.isNull()).toBeNull();
+})
