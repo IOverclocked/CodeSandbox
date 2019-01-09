84832f1c4a0754c1fadf4708978435fe3e1a42d6
IOverclocked <osiatycki.m@gmail.com>
2019-01-09 20:09:47 +0100

    simple todolist

--- react-crash-todo/src/App.js
+++ react-crash-todo/src/App.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos.jsx';
+import TodoAdd from './components/TodoAdd';
+import Header from './components/layout/Header.jsx';
+import uuid from 'uuid';
+
 import './App.css';
 
 class App extends Component {
+    state = {
+        todos: [{
+            id: uuid.v4(),
+            title: 'title1',
+            completed: false
+        }, {
+            id: uuid.v4(),
+            title: 'title2',
+            completed: true
+        }, {
+            id: uuid.v4(),
+            title: 'title3',
+            completed: false
+        }]
+    }
+
+
+    toggleTodo = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        })
+    }
+
+    deleteTodo = (id) => {
+        this.setState({
+            todos: [...this.state.todos.filter(todo => todo.id !== id)]
+        })
+    }
+
+    addTodo = (title) => {
+        const newTodo = {
+            id: uuid.v4(),
+            title,
+            completed: false
+        }
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    }
+
     render() {
+
+        console.table(...this.state.todos);
+
         return (
             <div className="App">
-                <h1>App</h1>
+                <div className="container">
+
+                </div>
+                <Header />
+                <TodoAdd addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} toggleTodo={this.toggleTodo} deleteTodo={this.deleteTodo} />
             </div>
         );
     }
