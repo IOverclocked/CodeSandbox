1d6d57f011bbab6b948e41b6e53d35bbd02ef71a
IOverclocked <osiatycki.m@gmail.com>
2019-01-21 21:13:45 +0100

    Initial

---
+++ react-my-todo/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import TodoList from './layout/TodoList';
+
+const GlobalStyled = createGlobalStyle`
+    body {
+        background-color: #2c3643;
+
+        *, *::before, *::after {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+    }
+`
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    font-size: 16px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+`
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <GlobalStyled />
+                <AppContainer>
+                    <TodoList />
+                </AppContainer>
+            </>
+        );
+    }
+}
+
+export default App;
